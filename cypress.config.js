e0ca5ade8d661b6009760f1386deac602d66bb93
tikoot <tiko.otarashvili99@gmail.com>
2022-11-08 07:29:16 +0100

    feat:install necessary packages for the  project

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,6 @@
+//eslint-disable-next-line no-undef
 const { defineConfig } = require("cypress");
-
+//eslint-disable-next-line no-undef
 module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
