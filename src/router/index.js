0186ed07911e3b75e89cd20008e87a4fd2777027
tikoot <tiko.otarashvili99@gmail.com>
2022-11-08 10:45:17 +0100

    feat:add navigation menu markup

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,27 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import LandingPage from "@/views/LandingPage.vue";
+import RegisterPage from "@/views/RegisterPage.vue";
+import LoginPage from "@/views/LoginPage.vue";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      component: LandingPage,
+      children: [
+        {
+          path: "/register",
+          component: RegisterPage,
+        },
+        {
+          path: "/login",
+          component: LoginPage,
+        },
+      ],
+    },
+  ],
 });
 
 export default router;
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h-[100vh] bg-[#11101a]">
+    <the-navbar></the-navbar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import TheNavbar from "@/components/TheNavbar.vue";
+export default {
+  components: {
+    TheNavbar,
+  },
+};
+</script>
