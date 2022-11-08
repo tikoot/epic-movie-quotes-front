210b39875d0cfe823d7a4cd60355d8f483e82aed
tikoot <tiko.otarashvili99@gmail.com>
2022-11-08 18:33:26 +0100

    feat: create dialog component for pop up pages

--- src/main.js
+++ src/main.js
@@ -6,7 +6,11 @@ import router from "./router";
 
 import "./assets/styles.css/index.css";
 
+import BaseDialog from "@/components/UI/BaseDialog.vue";
+
 const app = createApp(App);
+
+app.component("BaseDialog", BaseDialog);
 
 app.use(createPinia());
 app.use(router);
