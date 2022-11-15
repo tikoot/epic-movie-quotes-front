4dc274974e17c8320455406de681a2f6adbc2863
tikoot <tiko.otarashvili99@gmail.com>
2022-11-15 21:58:28 +0100

    feat:verify email with route params

---
+++ src/views/VerifyEmail.vue
@@ -0,0 +1,35 @@
+<template>
+  <base-dialog>
+    <div class="flex flex-col items-center pt-[40px]">
+      <img
+        src="@/assets/images/Vectorcheck.png"
+        alt="arrow image"
+        class="pb-[20px]"
+      />
+      <h1 class="text-white text-[32px] pb-[31px]">Thank you!</h1>
+      <p class="text-white text-center pb-[40px]">
+        Your account has been activated.
+      </p>
+      <button
+        class="w-full rounded-[4px] py-[7px] px-[13px] text-center text-white bg-[#E31221] mt-[8px]"
+      >
+        Go to my news feed
+      </button>
+    </div>
+  </base-dialog>
+</template>
+
+<script>
+import axios from "@/config/axios/axios.js";
+export default {
+  created() {
+    axios
+      .get("email-verification", {
+        params: this.$route.query,
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
