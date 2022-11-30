effdd5d894090dc5aa1ed961b0068e388cbbe300
tikoot <tiko.otarashvili99@gmail.com>
2022-11-30 18:46:23 +0100

    chore:register new components

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import router from "./router";
 import "./assets/styles.css/index.css";
 import "./config/vee-validate/rules";
 import "./config/vee-validate/messages";
+import i18n from "@/i18n/index.js";
 import BaseDialog from "@/components/authComponents/BaseDialog.vue";
 import PageBaseComponent from "@/components/newsFeedComponents/PageBaseComponent.vue";
+import PageBaseDialog from "@/components/newsFeedComponents/PageBaseDialog.vue";
+import LanguageSelect from "@/components/shared/LanguageSelect.vue";
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -17,8 +20,11 @@ pinia.use(({ store }) => {
 
 app.component("BaseDialog", BaseDialog);
 app.component("PageBaseComponent", PageBaseComponent);
+app.component("PageBaseDialog", PageBaseDialog);
+app.component("LanguageSelect", LanguageSelect);
 
 app.use(pinia);
 app.use(router);
+app.use(i18n);
 
 app.mount("#app");
