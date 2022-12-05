ff7dddef3f8839d323908dbd853fbe3c14f10598
tikoot <tiko.otarashvili99@gmail.com>
2022-12-06 00:29:58 +0100

    feat: delete movie

--- src/components/newsFeedComponents/CrudButtons.vue
+++ src/components/newsFeedComponents/CrudButtons.vue
@@ -10,8 +10,30 @@
       />
     </router-link>
     <img src="@/assets/images/Line8.png" alt="line" class="px-[25px]" />
-    <button>
+    <button @click="deleteMovie">
       <img src="@/assets/images/VectorDelete.png" alt="delete icon" />
     </button>
   </div>
 </template>
+
+<script setup>
+import axiosInstance from "@/config/axios/axios";
+import { useRouter } from "vue-router";
+import { useMovieStore } from "../../stores/addMovie";
+
+const router = useRouter();
+const store = useMovieStore();
+
+const movieId = store.movie_description[0].id;
+const deleteMovie = async () => {
+  axiosInstance
+    .delete("movies/" + movieId)
+    .then((response) => {
+      console.log(response);
+      router.replace({ name: "movieList" });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+</script>
